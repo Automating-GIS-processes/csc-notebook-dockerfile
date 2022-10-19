1856ce78187af69bd3d855e0ab271c58967ed3fc
Christoph Fink <christoph.fink@gmail.com>
2022-10-19 16:32:58 +0300

    simply check out the notebooks

--- checkout-autogis-lesson-notebooks.sh
+++ checkout-autogis-lesson-notebooks.sh
@@ -4,13 +4,4 @@
 trap "exec /usr/local/bin/start-notebook.sh $*" EXIT
 
 AUTOGIS_NOTEBOOKS="https://github.com/Automating-GIS-processes/notebooks.git"
-
-git clone "${AUTOGIS_NOTEBOOKS}" \
-|| (
-    NOTEBOOK_DIRECTORY=$(sed 's/.*\/\(.*\).git/\1/' <<<${AUTOGIS_NOTEBOOKS})
-    cd "${NOTEBOOK_DIRECTORY}"
-
-    # forget local changes
-    git stash
-    git pull
-)
+git clone "${AUTOGIS_NOTEBOOKS}"
